fad6544ef255b67425a5617faa48bea2292a1cdf
Dragon-S1 <cs19b052@smail.iitm.ac.in>
2023-03-21 22:43:17 +0530

    update file path, reduced disk read

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests as r
-import urllib3
+from getpass import getpass
 
 # subvert ssl errors: thanks to https://stackoverflow.com/a/41041028 [user: Larry Cai, edited: Qlimax]
-r.packages.urllib3.disable_warnings ()
+r.packages.urllib3.disable_warnings()
 r.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
 
 try:
@@ -12,70 +12,75 @@ except AttributeError:
 # end of external code
 
 PASS = 1
-FAIL_AUTH = -1
-FAIL_CONNECT = -2
+FAIL_CONNECT = -1
+FAIL_AUTH = -2
+FAIL_REQUEST = -3
 
 # approval method
-def approve (username, password):
-	S = r.Session ()
+def approve(username, password):
+	S = r.Session()
 	try:
 		# login
-		x = S.post ('https://netaccess.iitm.ac.in/account/login', data={'userLogin':username,'userPassword':password,'submit':''}, verify=False)
+		x = S.post('https://netaccess.iitm.ac.in/account/login', data={'userLogin':username,'userPassword':password,'submit':''}, verify=False)
 
 		# check login
-		if (str (x.content).find ('Authorized') == -1):
+		if str(x.content).find('Authorized') == -1:
 			# failed
 			return FAIL_AUTH
 	
 		# approve (1 day)
-		y = S.post ('https://netaccess.iitm.ac.in/account/approve', data={'duration':'2','approveBtn':''}, verify=False)
+		y = S.post('https://netaccess.iitm.ac.in/account/approve', data={'duration':'2','approveBtn':''}, verify=False)
 
 		# check approval
-		if (str (y.content).find ('Authorized') == -1):
+		if str(y.content).find('Authorized') == -1:
 			return FAIL_AUTH
 
 		return PASS
-	except ConnectionError:
-		return FAIL_CONNECT
+	except r.ConnectionError:
+		return FAIL_REQUEST
 
 
 # does cred file exist?
-import os, sys, json
-MYPATH = os.path.realpath (sys.argv [0])
-MYPATH = MYPATH [0 : MYPATH.rfind ('/')]
+import os, json
+MYPATH = os.path.dirname(__file__)+'/'
+cred_file = 'cred.json'
+FILEPATH = MYPATH + cred_file
 
-if not os.path.isfile (MYPATH + '/cred.json'):
+cred = {}
+
+try:
+# read cred file
+	with open(FILEPATH, 'r') as inf:
+		cred = json.load(inf)
+except FileNotFoundError:
 	# create cred file
-	print ('Enter your LDAP username: ', end='')
-	username = input ()
-	print ('Enter your LDAP password: ', end='')
-	password = input ()
+	print('Enter your LDAP username: ', end='')
+	username = input()
+	password = getpass("Enter your LDAP password: ")
 
 	cred = {'username':username, 'password':password}
 	
-	with open (MYPATH + '/cred.json', 'w') as outf:
-		json.dump (cred, outf)
+	with open(FILEPATH, 'w') as outf:
+		json.dump(cred, outf)
 
-	print (f"Saved credentials (as {MYPATH + '/cred.json'}).")
-
-# read cred file
-cred = {}
-with open (MYPATH + '/cred.json', 'r') as inf:
-	cred = json.load (inf)
+	print(f"Saved credentials (as {FILEPATH}).")
 
 # begin auth
-print ('Authorizing... ', end = '', flush=True)
+print('Authorizing... ', end = '', flush=True)
 
-v = approve (cred ['username'], cred ['password'])
+v = approve(cred['username'], cred['password'])
 
 # feedback
-if (v == PASS):
-	print ('Done.')
-elif (v == FAIL_AUTH):
+if v == PASS:
+	print('Done.')
+elif v == FAIL_CONNECT:
+	print('Failed.')
+	print('> Request failed. Are you connected?')
+elif v == FAIL_AUTH:
 	# remove cred file on auth failure
-	print ('Failed.')
-	print ('> Authorization failed. Removing stored credentials. Please run again.')
-	os.remove (MYPATH + '/cred.json')
-elif (v == FAIL_CONNECT):
-	print ('Failed.')
-	print ('> Request failed. Are you connected?')
+	print('Failed.')
+	print('> Authorization failed. Removing stored credentials. Please run again.')
+	os.remove(FILEPATH)
+elif v == FAIL_REQUEST:
+	print('Failed.')
+	print('> Request failed. Are you connected to IITM Lan?')
